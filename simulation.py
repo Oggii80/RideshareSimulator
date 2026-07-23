d416ede4f2e39a4666daec8da4c7bf45baecdc45
Oggii80 <82605916+Oggii80@users.noreply.github.com>
2026-07-22 21:32:13 -0500

    Modify Simulation constructor to accept map filename
    Initialize Simulation class with map filename and load graph.

--- simulation.py
+++ simulation.py
@@ -4,7 +4,14 @@ This class serves as the main controller for the entire simulation.
 As of this submission, it currently holds collections of cars and riders
 """
 
+import os
+from graph import Graph
+
 class Simulation:
-    def __init__(self):
+    def __init__(self, map_filename):
         self.cars={}
         self.riders={}
+        self.map = Graph()
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        self.map.load_from_file(os.path.join(base_dir, map_filename))
+        
