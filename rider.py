1a042adf8bf820feddd4fc8e8a9d7311501ccaa2
Oggii80 <82605916+Oggii80@users.noreply.github.com>
2026-08-12 19:39:42 -0500

    Enhance Rider class with additional time attributes
    Added request_time, pickup_time, and dropoff_time attributes to the Rider class.

--- rider.py
+++ rider.py
@@ -4,11 +4,14 @@ Represent a single rider in the simulation
 """
 
 class Rider:
-    def __init__(self, rider_id, pickup_location, dropoff_location):
+    def __init__(self, rider_id, pickup_location, dropoff_location, request_time=0):
         self.id=rider_id
         self.start_location=pickup_location
         self.destination=dropoff_location
         self.status="waiting"
+        self.request_time=0
+        self.pickup_time=0
+        self.dropoff_time=0
 
     def __str__(self):
         return(
